402d64d75cf25bdd52b7d28ee4422212acf7ca20
aman071100 <amanverma071100@gmail.com>
2026-07-21 19:50:55 +0530

    Added authentication UI and implemented logout functionality

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/Sidebar/Sidebar'
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MainLayout from './layouts/MainLayout';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/AppRoutes';
 
 function App() {
   return (
     <>
-      <div>
-        <Navbar />
-        <Sidebar />
-      </div>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </>
   )
 }
---
+++ frontend/react-app/src/services/httpService.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import NotificationService from "./NotificationService";
+import { toast } from "react-toastify";
+
+const base_url = import.meta.env.VITE_BASE_URL;
+
+const api = axios.create({
+    baseURL: base_url,
+    withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (
+            error.response?.status === 401 &&
+            !originalRequest._retry &&
+            !originalRequest.url?.includes("/auth/login") &&
+            !originalRequest.url?.includes("/auth/refresh")
+        ) {
+            originalRequest._retry = true;
+
+            if (!originalRequest) {
+                return Promise.reject(error);
+            }
+
+            try {
+                const res = await api.post("/auth/refresh");
+                const newAccessToken = res.data.accessToken;
+
+                localStorage.setItem("token", newAccessToken);
+
+                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+                return api(originalRequest);
+            } catch (err) {
+                localStorage.removeItem("token");
+                window.location.href = import.meta.env.VITE_LOGIN_URL;
+                return Promise.reject(err);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+const makeHttpRequest = async ({ method, url, data = {}, params = {} }) => {
+    try {
+        const res = await api({
+            method,
+            url,
+            data,
+            params,
+        });
+
+        if (res?.data?.message) {
+            NotificationService.success(res?.data?.message);
+        }
+
+        return res?.data;
+    } catch (error) {
+        NotificationService.error(
+            error?.response?.data?.error || "Something went wrong"
+        );
+        throw error;
+    }
+};
+
+export default makeHttpRequest;
